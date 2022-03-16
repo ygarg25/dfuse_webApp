8ef2e46984290e73de80710073b038694e725c36
ygarg25 <ygarg25@gmail.com>
2022-03-16 06:42:01 +0530

    add webapp

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Switch, Route, Redirect, Routes, Navigate } from "react-router-dom";
+import Navbar from "./Component/navbar";
+import HomeScreen from "./Component/home";
+// import Show from "./show";
+// import NewStudent from "./newStud";
+// import EditStudent from "./editStudent";
+
+function App() {
+  return (
+    <div className=''>
+      <Navbar />
+      {/* <HomeScreen /> */}
+      <div>
+        <Routes>
+          {/* <Route path="/student/:name" component={EditStudent} /> */}
+          {/* <Route path="/allStudent" render={props => <Show {...props} />} /> */}
+          <Route path="/home" element={<HomeScreen />} />
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          {/* <Redirect from="/" to="/home" /> */}
+          {/* <Redirect to="/home" /> */}
+          {/* <Navigate to="/home" /> */}
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/try_navbar.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ paddingLeft: '10px ' }}>
+            {/* <div class="container"> */}
+            <Link className="navbar-brand " to="/allStudent">
+                dFuse
+            </Link>
+            {/* </div> */}
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto ">
+                    <li className="nav-item ">
+                        <Link className="nav-link " to="/newStudent">
+                            {/* New Student */}
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
