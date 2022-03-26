c19fc58f540533f58e41ac0e407a2de9a84adb7c
ygarg25 <ygarg25@gmail.com>
2022-03-26 15:21:05 +0530

    update design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import { Switch, Route, Redirect, Routes, Navigate } from "react-router-dom";
-import Navbar from "./Component/navbar";
 import HomeScreen from "./Component/home";
-// import Show from "./show";
-// import NewStudent from "./newStud";
-// import EditStudent from "./editStudent";
+import Navbar from "./Component/Navbar/navbar";
+import About from "./Component/about";
+import Strategies from "./Component/strategies";
+import Risk_Parity from './Component/risk_Parity';
+import Tokenomics from './Component/tokenomics';
+import Teams from './Component/teams';
+import './Css/App.css';
 
 function App() {
   return (
     <div className=''>
-      {/* <Navbar /> */}
+      <Navbar />
       {/* <HomeScreen /> */}
       <div>
         <Routes>
           {/* <Route path="/student/:name" component={EditStudent} /> */}
           {/* <Route path="/allStudent" render={props => <Show {...props} />} /> */}
           <Route path="/home" element={<HomeScreen />} />
+          <Route path="/howItWorks" element={<About />} />
+          <Route path="/strategies" element={<Strategies />} />
+          <Route path="/risk" element={<Risk_Parity />} />
+          <Route path="/tokenomics" element={<Tokenomics />} />
+          <Route path="/teams" element={<Teams />} />
           <Route path="/" element={<Navigate replace to="/home" />} />
-          {/* <Redirect from="/" to="/home" /> */}
-          {/* <Redirect to="/home" /> */}
-          {/* <Navigate to="/home" /> */}
         </Routes>
       </div>
     </div>
--- src/Component/home.jsx
+++ src/Component/home.jsx
@@ -1,86 +1,60 @@
 import React, { Component } from 'react';
-import Navbar from './navbar';
-import About from './about';
-import Strategies from './strategies';
-import Risk_Parity from './risk_Parity';
-import Tokenomics from './tokenomics';
-import Teams from './teams';
 import "./../Css/main.css"
 
 
 class HomeScreen extends Component {
     render() {
-        return <div className=''>
-            <div className="row " style={{
-                backgroundImage: `url("/web-Banner.jpeg")`,
-                backgroundRepeat: 'no-repeat',
-                // width: '100%'
-                backgroundSize: 'cover',
-                height: '120vh',
-            }}>
-                <Navbar />
-                <div className='col-1'></div>
-                <div className="col-md-4 col-sm-5 p-5 pt-0 text-white">
-                    <h2>Hi! We're <br /> dFuse</h2>
-                    <p
-                        // className="text-justify"
-                        style={{
-                            margin: '0 0 13px',
-                            // color: 'black',
-                            display: 'inline-block',
-                            fontSize: '14px',
-                            fontWeight: '400',
-                            lineHeight: '1.7em',
-                            textAlign: 'justify'
-                        }}
-                    >
+        return <div
+            className='home-bg'
+        >
+            <div className="row align-middle " style={{ margin: "5em" }}>
+                <div className='col-md-1 col-sm-none '></div>
+                <div className="col-md-4 col-sm-12 p-sm-2 ">
+                    <br />
+                    <br />
+
+                    <span className="heading1">Hi! We're dFuse</span>
+                    <p>
                         <br />
-                        <h5>An Interchain Yield Farming Protocol built on Juno.</h5>
+                        <span className="heading2" >An Interchain Yield Farming Protocol built on Juno.</span>
                         <br />
-                        dFuse is the simplest way to make money on your IBC assets.dFuse allows you to deploy your
-                        ATOM and other tokens into vaults that earn yield through multiple strategies across the various
-                        IBC protocols.dFuse takes care of all the hard parts like protecting against Impermanent loss,
-                        maintaining a healthy collateral ratio, auto compounding and rebalancing your portfolio across
-                        multiple opportunites in multiple chains.We dFuse the risks and stress of DeFi investing.
+                        <br />
+                        <span className="p_text">
+                            dFuse is the simplest way to make money on your IBC assets.dFuse allows you to deploy your
+                            ATOM and other tokens into vaults that earn yield through multiple strategies across the various
+                            IBC protocols. dFuse takes care of all the hard parts like protecting against Impermanent loss,
+                            maintaining a healthy collateral ratio, auto compounding and rebalancing your portfolio across
+                            multiple opportunites in multiple chains.We dFuse the risks and stress of DeFi investing.
+                        </span>
                     </p>
+                    <br />
+                    <br />
                     <a
-                        href="#HowItWorks"
+                        href="/howItWorks"
                     >
-                        <button type="button" class="btn btn-danger">More About Us</button>
+                        <button type="button" class="button-1">More About Us</button>
                     </a>
                 </div>
                 <div
                     // className='col-md-7 image_block'
-                    className='col-md-7 '
+                    className='col-md-7 col-sm-12 text-center'
+                    style={{
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}
                 >
-                    {/* <img
-                        // src="https://www.larvalabs.com/public/images/mattjohn-homepage-2x.png"
-                        // src="https://thumbs.dreamstime.com/z/soccer-stuff-182425593.jpg"
-                        // src={require('../Assets/dFuse_bomb.jpeg')}
-                        // width='800'
-                        // height='600'
-                        width="1000"
-                        height="300"
-                        // height='auto'
+                    <img
+                        src={require('../Assets/astronaut-PNG.png')}
+                        className="align-middle astro_image"
                         alt="dFuse"
-                        style={{
-                            maxWidth: "100%",
-                            height: "85%"
-                        }}
-                    /> */}
+                        align="middle"
+
+                    />
                 </div>
             </div>
+        </div>
 
-            <About />
 
-            <Strategies />
-
-            <Risk_Parity />
-
-            <Tokenomics />
-
-            <Teams />
-        </div >;
     }
 }
 
